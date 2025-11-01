11157764312ca30d6bf516febb065bfbb6c195e3
Victor Hugh <victorhughsilva@outlook.com>
2025-11-01 01:20:26 +0000

    ai_core

---
+++ ai_core/mcp_tools.py
@@ -0,0 +1,47 @@
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_ollama import ChatOllama
+
+from ai_core.graph_logic import create_graph_with_tools
+from langchain_core.messages import HumanMessage
+
+async def connect_to_server(user_input):
+    server_params = StdioServerParameters(
+        command="python",
+        args=["ai_core/server.py"],  # Caminho para servidor MCP
+    )
+    try:
+        async with stdio_client(server_params) as (read, write):
+            async with ClientSession(read, write) as session:
+                await session.initialize()
+                
+                # teste das ferramentas na sessão
+                # tools_result = await session.list_tools()
+                # print("Available tools:")
+                # for tool in tools_result.tools:
+                #     print(f"  - {tool.name}: {tool.description}")
+
+                tools = await load_mcp_tools(session)
+                model = ChatOllama(model="qwen3:1.7b").bind_tools(tools)
+                app = create_graph_with_tools(tools)
+
+                inputs = {
+                "messages": user_input,
+                "model": model,
+                "tools": tools,
+                "session": session,
+                }
+                async for state in app.astream(inputs, stream_mode="values"):
+                    msgs = state.get("messages", [])
+                    if not msgs:
+                        continue
+                    last = msgs[-1]
+                    if getattr(last, "tool_calls", None) or not last.content.strip():
+                        #print("🤖 → pediu tool:", last.tool_calls[0]["name"])
+                        continue
+                    final_result = last.content
+            return final_result
+    except Exception as e:
+        print(f"Erro ao conectar com MCP: {e}")
+        raise
---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import asyncio
+from ai_core.mcp_tools import connect_to_server
+from langchain_core.messages import HumanMessage, AIMessage
+
+st.set_page_config(page_title="Agente com MCP", layout="centered")
+st.title("🤖 LPhantom")
+
+user_input = st.chat_input("Digite algo...")
+
+if user_input:
+    result = asyncio.run(connect_to_server(user_input))
+    st.chat_message("user").write(user_input)
+    st.chat_message("assistant").write(f"Echo: {result}")
+
