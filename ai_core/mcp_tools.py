f53fb562bed400d0c5b46578a304b3a5d525888e
Victor Hugh <victorhughsilva@outlook.com>
2025-11-01 01:28:11 +0000

    ai_core-teste

--- ai_core/mcp_tools.py
+++ ai_core/mcp_tools.py
@@ -2,28 +2,26 @@ from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_ollama import ChatOllama
-
 from ai_core.graph_logic import create_graph_with_tools
-from langchain_core.messages import HumanMessage
+import re
 
-async def connect_to_server(user_input):
+async def connect_to_server(user_input,mcp_path='ai_core/server.py',ollama_model='qwen3:1.7b'):
     server_params = StdioServerParameters(
         command="python",
-        args=["ai_core/server.py"],  # Caminho para servidor MCP
+        args=[mcp_path],  # Path for MCP local server
     )
     try:
         async with stdio_client(server_params) as (read, write):
             async with ClientSession(read, write) as session:
                 await session.initialize()
                 
-                # teste das ferramentas na sessão
                 # tools_result = await session.list_tools()
                 # print("Available tools:")
                 # for tool in tools_result.tools:
                 #     print(f"  - {tool.name}: {tool.description}")
 
                 tools = await load_mcp_tools(session)
-                model = ChatOllama(model="qwen3:1.7b").bind_tools(tools)
+                model = ChatOllama(model=ollama_model).bind_tools(tools)
                 app = create_graph_with_tools(tools)
 
                 inputs = {
@@ -41,7 +39,8 @@ async def connect_to_server(user_input):
                         #print("🤖 → pediu tool:", last.tool_calls[0]["name"])
                         continue
                     final_result = last.content
+            final_result = re.sub(r"<think>.*?</think>", "", final_result, flags=re.DOTALL).strip()
             return final_result
     except Exception as e:
-        print(f"Erro ao conectar com MCP: {e}")
+        print(f"Error connecting to MCP: {e}")
         raise
